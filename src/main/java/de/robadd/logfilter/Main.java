65fbf19f354e1c3eebe0d169a78413fca61701b3
RoK <robert.kraus@speed4trade.com>
2021-05-20 08:00:32 +0200

    added todos

--- src/main/java/de/robadd/logfilter/Main.java
+++ src/main/java/de/robadd/logfilter/Main.java
@@ -10,34 +10,32 @@ import de.robadd.logfilter.ui.MainWindow;
 public class Main
 {
 
-	public static void main(final String[] args)
-	{
-		try
-		{
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		}
-		catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e1)
-		{
-			e1.printStackTrace();
-		}
-		EventQueue.invokeLater(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				try
-				{
-					MainWindow window = new MainWindow();
-					window.setFrameVisible();
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		});
+    public static void main(final String[] args)
+    {
+        // TODO close filehandler on write
+        // TODO log throwable
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e1)
+        {
+            e1.printStackTrace();
+        }
+        EventQueue.invokeLater(() ->
+        {
+            try
+            {
+                MainWindow window = new MainWindow();
+                window.setFrameVisible();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        });
 
-	}
+    }
 
 }
