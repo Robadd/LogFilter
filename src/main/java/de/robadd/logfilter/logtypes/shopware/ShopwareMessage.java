9d69bae19a8c0f72c129e4ea2aca2e35f8151d6a
RoK <robert.kraus@speed4trade.com>
2021-05-21 14:18:56 +0200

    Removed apache commons

--- src/main/java/de/robadd/logfilter/logtypes/shopware/ShopwareMessage.java
+++ src/main/java/de/robadd/logfilter/logtypes/shopware/ShopwareMessage.java
@@ -3,39 +3,39 @@ package de.robadd.logfilter.logtypes.shopware;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
+import com.jgoodies.common.base.Strings;
 
 import de.robadd.logfilter.model.Message;
 
 public class ShopwareMessage implements Message
 {
-	private String requestMethod;
-	private String msg;
+    private String requestMethod;
+    private String msg;
 
-	@Override
-	public void character(final String str)
-	{
-		if (!StringUtils.isBlank(str))
-		{
-			msg = str;
-			Pattern pattern = Pattern.compile("([\\w-]*)");
-			Matcher matcher = pattern.matcher(str);
-			if (matcher.find())
-			{
-				requestMethod = matcher.group(0);
-			}
-		}
-	}
+    @Override
+    public void character(final String str)
+    {
+        if (!Strings.isBlank(str))
+        {
+            msg = str;
+            Pattern pattern = Pattern.compile("([\\w-]*)");
+            Matcher matcher = pattern.matcher(str);
+            if (matcher.find())
+            {
+                requestMethod = matcher.group(0);
+            }
+        }
+    }
 
-	@Override
-	public String getRequestMethod()
-	{
-		return requestMethod;
-	}
+    @Override
+    public String getRequestMethod()
+    {
+        return requestMethod;
+    }
 
-	@Override
-	public String toString()
-	{
-		return msg;
-	}
+    @Override
+    public String toString()
+    {
+        return msg;
+    }
 }
--- src/main/java/de/robadd/logfilter/ui/filter/DateFilterPanel.java
+++ src/main/java/de/robadd/logfilter/ui/filter/DateFilterPanel.java
@@ -2,14 +2,11 @@ package de.robadd.logfilter.ui.filter;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import javax.swing.JLabel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
-
-import org.apache.commons.lang3.tuple.Pair;
 
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
@@ -18,91 +15,91 @@ import com.jgoodies.forms.layout.RowSpec;
 
 import de.robadd.logfilter.model.Index;
 
-public class DateFilterPanel extends UnimplementedFilterPanel<Pair<Calendar, Calendar>>
+public class DateFilterPanel extends UnimplementedFilterPanel<TimeSpan>
 {
 
-	private JTextField fromTextField;
-	private JTextField toTextField;
+    private JTextField fromTextField;
+    private JTextField toTextField;
 
-	public DateFilterPanel()
-	{
-		super();
-	}
+    public DateFilterPanel()
+    {
+        super();
+    }
 
-	private static final long serialVersionUID = -4111684591269597533L;
+    private static final long serialVersionUID = -4111684591269597533L;
 
-	@Override
-	public String getTitle()
-	{
-		return "Datum";
-	}
+    @Override
+    public String getTitle()
+    {
+        return "Datum";
+    }
 
-	@Override
-	public List<Pair<Calendar, Calendar>> getSelectedValues()
-	{
-		return new ArrayList<>();
-	}
+    @Override
+    public List<TimeSpan> getSelectedValues()
+    {
+        return new ArrayList<>();
+    }
 
-	@Override
-	public void setValues(final List<Pair<Calendar, Calendar>> values)
-	{
-		// Do nothing
-	}
+    @Override
+    public void setValues(final List<TimeSpan> values)
+    {
+        // Do nothing
+    }
 
-	@Override
-	protected void init()
-	{
-		setLayout(new FormLayout(new ColumnSpec[]
-		{ FormSpecs.UNRELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode(
-			"default:grow"), FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("default:grow"),
-				FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.UNRELATED_GAP_COLSPEC, }, new RowSpec[]
-		{ FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.MIN_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.MIN_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC, }));
+    @Override
+    protected void init()
+    {
+        setLayout(new FormLayout(new ColumnSpec[]
+        {FormSpecs.UNRELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode(
+                "default:grow"), FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("default:grow"),
+                FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.UNRELATED_GAP_COLSPEC,}, new RowSpec[]
+        {FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.MIN_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.MIN_ROWSPEC,
+                FormSpecs.RELATED_GAP_ROWSPEC,}));
 
-		JLabel fromLabel = new JLabel("Von:");
-		add(fromLabel, "2, 2");
+        JLabel fromLabel = new JLabel("Von:");
+        add(fromLabel, "2, 2");
 
-		JSlider fromTime = new JSlider();
-		add(fromTime, "4, 2");
+        JSlider fromTime = new JSlider();
+        add(fromTime, "4, 2");
 
-		fromTextField = new JTextField();
-		add(fromTextField, "6, 2, fill, default");
-		fromTextField.setColumns(10);
+        fromTextField = new JTextField();
+        add(fromTextField, "6, 2, fill, default");
+        fromTextField.setColumns(10);
 
-		JLabel ToLabel = new JLabel("Bis:");
-		add(ToLabel, "2, 4");
+        JLabel ToLabel = new JLabel("Bis:");
+        add(ToLabel, "2, 4");
 
-		JSlider toTime = new JSlider();
-		add(toTime, "4, 4");
+        JSlider toTime = new JSlider();
+        add(toTime, "4, 4");
 
-		toTextField = new JTextField();
-		toTextField.setColumns(10);
-		add(toTextField, "6, 4, fill, default");
-	}
+        toTextField = new JTextField();
+        toTextField.setColumns(10);
+        add(toTextField, "6, 4, fill, default");
+    }
 
-	@Override
-	public void setValuesFromIndex(final Index index)
-	{
-		// TODO implement
-		// values = index.getLogLevels().stream().collect(Collectors.toList());
-	}
+    @Override
+    public void setValuesFromIndex(final Index index)
+    {
+        // TODO implement
+        // values = index.getLogLevels().stream().collect(Collectors.toList());
+    }
 
-	@Override
-	public Method getEventMethod(final Class<?> clazz)
-	{
-		try
-		{
-			return clazz.getMethod("getTimestamp");
-		}
-		catch (NoSuchMethodException | SecurityException e)
-		{
-			return null;
-		}
-	}
+    @Override
+    public Method getEventMethod(final Class<?> clazz)
+    {
+        try
+        {
+            return clazz.getMethod("getTimestamp");
+        }
+        catch (NoSuchMethodException | SecurityException e)
+        {
+            return null;
+        }
+    }
 
-	@Override
-	public boolean isImplemented()
-	{
-		return false;
-	}
+    @Override
+    public boolean isImplemented()
+    {
+        return false;
+    }
 }
